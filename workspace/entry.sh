f2f42ae64e2f763ba99fe70bddf0234d275baf7e
JacobLinCool <jacoblincool@gmail.com>
2022-08-09 01:26:21 +0800

    feat(workspace): run dockerd in entry.sh

--- workspace/entry.sh
+++ workspace/entry.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -eu
 
+# Run dockerd if it's not running
+docker info > /dev/null 2>&1 || sudo bash -c 'dockerd > /dockerd.log 2>&1 &' || true
+
 # If no args are given
 if [ $# -eq 0 ]; then
   set -- /bin/bash
