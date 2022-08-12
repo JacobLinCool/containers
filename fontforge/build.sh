ad261164b19c3bc380ba835b8e69e428fe790f31
JacobLinCool <jacoblincool@gmail.com>
2022-08-12 22:42:44 +0800

    build: copy images to docker hub

--- fontforge/build.sh
+++ fontforge/build.sh
@@ -4,3 +4,6 @@ set -ex
 cd $(dirname $0)
 
 docker buildx bake --push --set "*.platform=linux/arm64/v8,linux/amd64"
+
+regctl -v info image copy ghcr.io/jacoblincool/fontforge jacoblincool/fontforge
+regctl -v info image copy ghcr.io/jacoblincool/fontforge-convert jacoblincool/fontforge-convert
--- workspace/build.sh
+++ workspace/build.sh
@@ -4,3 +4,7 @@ set -ex
 cd $(dirname $0)
 
 docker buildx bake --push --set "*.platform=linux/arm64/v8,linux/amd64"
+
+regctl -v info image copy ghcr.io/jacoblincool/workspace-lite jacoblincool/workspace-lite
+regctl -v info image copy ghcr.io/jacoblincool/workspace jacoblincool/workspace
+regctl -v info image copy ghcr.io/jacoblincool/workspace-full jacoblincool/workspace-full
