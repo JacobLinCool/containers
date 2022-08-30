4aa84b1078231c8467438849cd6227fcf0ea007a
JacobLinCool <jacoblincool@gmail.com>
2022-08-30 18:15:49 +0800

    build(workspace): build and push workspace-pure

--- workspace/build.sh
+++ workspace/build.sh
@@ -5,6 +5,7 @@ cd $(dirname $0)
 
 docker buildx bake --push --set "*.platform=linux/arm64/v8,linux/amd64"
 
+regctl -v info image copy ghcr.io/jacoblincool/workspace-pure jacoblincool/workspace-pure
 regctl -v info image copy ghcr.io/jacoblincool/workspace-lite jacoblincool/workspace-lite
 regctl -v info image copy ghcr.io/jacoblincool/workspace jacoblincool/workspace
 regctl -v info image copy ghcr.io/jacoblincool/workspace-full jacoblincool/workspace-full
